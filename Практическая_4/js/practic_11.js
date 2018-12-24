9e548071bedc80a4f5e5d9a2f46a3a70abcb7dc2
zv-lena <loveskillet61@gmail.com>
2018-12-24 18:41:51 +0300

    completed 11 practic-work

--- Практическая_4/js/practic_11.js
+++ Практическая_4/js/practic_11.js
@@ -1,13 +1,19 @@
-// const canvas = document.querySelector('canvas')
-// const ctx = canvas.getContext('2d')
-// canvas.height = 400
-// canvas.width = 500
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext('2d')
+canvas.height = 140
+canvas.width = 280
+const randomRGBA = (min, max) => {
+    let rand = min + Math.random() * (max + 1 - min) 
+    rand = Math.floor(rand) 
+    return rand  
+}
+let gradient = ctx.createLinearGradient(0, 0, 0, 60)
 
-// let gradient = ctx.createLinearGradient(0, 0, 0, 60)
-// gradient.addColorStop(0.0, 'rgba(0, 0, 255, 1)')
-// gradient.addColorStop(0.3, 'rgba(128, 0, 255, 0.6)')
-// gradient.addColorStop(0.6, 'rgba(0, 0, 255, 0.4)')
-// gradient.addColorStop(1.0, 'rgba(0, 255, 0, 0.2)')
-// ctx.font = '70px sans-serif'
-// ctx.fillStyle = gradient
-// ctx.fillText(':-)', 80, 50)
+ console.log(randomRGBA(0, 200))
+gradient.addColorStop(0.0, `rgba(${randomRGBA(0, 200)}, ${randomRGBA(0, 200)}, ${randomRGBA(0, 200)}, ${Math.random()})`)
+gradient.addColorStop(0.5, `rgba(${randomRGBA(0, 200)}, ${randomRGBA(0, 200)}, ${randomRGBA(0, 200)}, ${Math.random()})`)
+gradient.addColorStop(1,   `rgba(${randomRGBA(0, 200)}, ${randomRGBA(0, 200)}, ${randomRGBA(0, 200)}, ${Math.random()})`)
+
+ctx.font = '70px sans-serif'
+ctx.fillStyle = gradient
+ctx.fillText(':-)', 80, 50)

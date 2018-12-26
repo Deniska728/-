5020bdb7437cd5e896f2f953714d48dd9c283d50
zv-lena <loveskillet61@gmail.com>
2018-12-26 20:05:13 +0300

    fixed bug

--- Практическая_4/js/practic_7.js
+++ Практическая_4/js/practic_7.js
@@ -1,7 +1,6 @@
 const photos = document.querySelector('.photos')
-const newArr = []
 
-shuffle = arr => { 
+const shuffle = arr => { 
   let j, temp
   for(let i = arr.length - 1; i > 0; i--){ 
     j = Math.floor(Math.random()*(i + 1)) 
@@ -19,6 +18,7 @@ photos.onclick = e =>{
   e.preventDefault()
   while(target != photos){
     if(target.getAttribute('data-img')){
+      const newArr = []
       let arrPhotos = document.querySelectorAll('[data-img="img"]')
       arrPhotos.forEach( img => {
          newArr.push(img.src)

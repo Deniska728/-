4a5383de0f829e97e893c1489fc223c6134d6c84
zv-lena <loveskillet61@gmail.com>
2018-12-26 19:40:14 +0300

    added a random change of pictures, the same pictures can be repeated

--- Практическая_4/js/forPasha.js
+++ Практическая_4/js/forPasha.js
@@ -1,23 +1,35 @@
 const photos = document.querySelector('.photos')
+const newArr = []
 
-const shuffleArray = array => {
-    for (let i = array.length - 1; i > 0; i--) {
-        let newArray = array.slice(0)
-        Array.from(newArray).sort( (a, b) => Math.random() - 0.5 )
-    }
+shuffle = arr => { 
+  let j, temp
+  for(let i = arr.length - 1; i > 0; i--){ 
+    j = Math.floor(Math.random()*(i + 1)) 
+    temp = arr[j]
+    console.log(arr[i])
+    arr[j] = arr[i]
+    arr[i] = temp
+    console.log(arr[i])
+  } 
+  return arr
 }
 
 photos.onclick = e =>{
-   let target = e.target
-   e.preventDefault()
-   while(target != photos){
-     if(target.getAttribute('data-img')){
-       const arrPhotos = document.querySelectorAll('[data-img="img"]')
-       shuffleArray(arrPhotos)
-       return
+  let target = e.target
+  e.preventDefault()
+  while(target != photos){
+    if(target.getAttribute('data-img')){
+      let arrPhotos = document.querySelectorAll('[data-img="img"]')
+      arrPhotos.forEach( img => {
+         newArr.push(img.src)
+      })
+      const imgArr = shuffle(newArr)
+      for (const img in arrPhotos){
+        arrPhotos[img].src = imgArr[img]
+      }
+      console.log(arrPhotos)
+      return
      }
      target = target.parentNode
-   }
+  }
 }
-
-
